0c9ae1942f61907006af6fab21224cc3adaeb256
Seegobin, Varindra <vseegobin3@gatech.edu>
2024-10-30 21:11:57 -0400

    Added abstract art and made base (somewhat) responsive to mobile.

--- UserAuth/settings.py
+++ UserAuth/settings.py
@@ -1,3 +1,4 @@
 from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 STATICFILES_DIRS = [BASE_DIR / 'static']
+ALLOWED_HOSTS = ['0.0.0.0', '127.0.0.1', 'localhost', '192.168.1.60']
