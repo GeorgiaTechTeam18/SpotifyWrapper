9f3f2faa5b36295950da30c7e50b12e60ccc6593
Theo <theomhalpern@gmail.com>
2024-11-18 14:01:21 -0500

    removed cobbab casing for my sanity

--- Wrapped/urls.py
+++ Wrapped/urls.py
@@ -4,13 +4,13 @@ from .views import post_wrap, view_wraps, create_wrap, like_wrap, view_wrap, sel
     view_public_wraps
 
 urlpatterns = [
-    path('post-wrap/', post_wrap, name='post_wrap'),
-    path('view-wraps/', view_wraps, name='view_wraps'),
-    path('create-wrap/', create_wrap, name='create_wrap'),
-    path('view-wrap/wrap_id_<uuid:wrap_id>/', view_wrap, name='view_wrap'),
-    path('like-wrap/<uuid:wrap_id>/', like_wrap, name='like_wrap'),
-    path('create-wrap/<time_range>/', create_wrap, name='create_wrap'),
-    path('select-wraps-to-post/', select_wraps_to_post, name='select_wraps_to_post'),
-    path('make-wraps-public/', make_wraps_public, name='make_wraps_public'),
-    path('view-public-wraps/', view_public_wraps, name='view_public_wraps')
+    path('post_wrap/', post_wrap, name='post_wrap'),
+    path('view_wraps/', view_wraps, name='view_wraps'),
+    path('create_wrap/', create_wrap, name='create_wrap'),
+    path('view_wrap/wrap_id_<uuid:wrap_id>/', view_wrap, name='view_wrap'),
+    path('like_wrap/<uuid:wrap_id>/', like_wrap, name='like_wrap'),
+    path('create_wrap/<time_range>/', create_wrap, name='create_wrap'),
+    path('select_wraps_to_post/', select_wraps_to_post, name='select_wraps_to_post'),
+    path('make_wraps_public/', make_wraps_public, name='make_wraps_public'),
+    path('view_public_wraps/', view_public_wraps, name='view_public_wraps')
 ]
