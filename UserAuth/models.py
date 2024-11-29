01424416ca542ee28d655eff1f97be5cb9c83684
dumax315 <26506274+dumax315@users.noreply.github.com>
2024-11-29 21:54:25 +0000

    Auto code format

--- UserAuth/models.py
+++ UserAuth/models.py
@@ -1,10 +1,19 @@
 from django.contrib.auth.models import AbstractUser, PermissionsMixin
 from django.db import models
 
+
 class User(AbstractUser, PermissionsMixin):
-    default_spotify_token = models.ForeignKey('SpotifyToken', on_delete=models.SET_NULL, related_name="default_spotify_token", null=True, blank=True)
+    default_spotify_token = models.ForeignKey(
+        "SpotifyToken",
+        on_delete=models.SET_NULL,
+        related_name="default_spotify_token",
+        null=True,
+        blank=True,
+    )
+
     def __str__(self):
         return f"{self.first_name} {self.last_name} - {self.email}"
+
 
 class ContactUs(models.Model):
     email = models.EmailField(max_length=254, verbose_name="User Email")
@@ -14,13 +23,18 @@ class ContactUs(models.Model):
     def __str__(self):
         return f"Message from {self.email} at {self.submitted_at}"
 
+
 class SpotifyToken(models.Model):
-    user = models.ForeignKey(User,
-                             models.SET_NULL,
-                             blank=True,
-                             null=True,)
-    spotify_account_email = models.CharField(max_length=255, null=True, blank=True)
-    spotify_account_username = models.CharField(max_length=255, null=True, blank=True)
+    user = models.ForeignKey(
+        User,
+        models.SET_NULL,
+        blank=True,
+        null=True,
+    )
+    spotify_account_email = models.CharField(
+        max_length=255, null=True, blank=True)
+    spotify_account_username = models.CharField(
+        max_length=255, null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     refresh_token = models.CharField(max_length=150)
     access_token = models.CharField(max_length=255)
